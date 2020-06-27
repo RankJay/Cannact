612d2f32cbef14f987c3e17427f5fe1bd79eb7af
JAY RANK <53647573+RankJay@users.noreply.github.com>
2020-06-27 14:51:24 +0530

    Add files via upload

---
+++ lib/custom4.dart
@@ -0,0 +1,441 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class custom4 extends StatelessWidget {
+  custom4({
+    Key key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffffffff),
+      body: Stack(
+        children: <Widget>[
+          Transform.translate(
+            offset: Offset(146.0, 550.0),
+            child: Container(
+              width: 94.0,
+              height: 93.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.elliptical(47.0, 46.5)),
+                color: const Color(0xff171717),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(0.0, 41.0),
+            child: Stack(
+              children: <Widget>[
+                Transform.translate(
+                  offset: Offset(140.5, 104.5),
+                  child: SvgPicture.string(
+                    _svg_94btrn,
+                    allowDrawingOutsideViewBox: true,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(146.0, 352.0),
+            child: Container(
+              width: 291.0,
+              height: 291.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.elliptical(145.5, 145.5)),
+                color: const Color(0xff000000),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(226.0, 386.0),
+            child: Text(
+              'C',
+              style: TextStyle(
+                fontFamily: 'Gilroy ☞',
+                fontSize: 180,
+                color: const Color(0xffffffff),
+                letterSpacing: 18,
+                fontWeight: FontWeight.w700,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(1661.0, 3832.0),
+            child: Container(
+              width: 365.0,
+              height: 365.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.elliptical(182.5, 182.5)),
+                color: const Color(0x6effffff),
+                boxShadow: [
+                  BoxShadow(
+                    color: const Color(0x47000000),
+                    offset: Offset(0, 0),
+                    blurRadius: 30,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(1701.0, 3872.0),
+            child: Container(
+              width: 285.0,
+              height: 285.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.elliptical(142.5, 142.5)),
+                color: const Color(0xffffffff),
+                boxShadow: [
+                  BoxShadow(
+                    color: const Color(0x47ffffff),
+                    offset: Offset(0, 0),
+                    blurRadius: 30,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(146.0, 1188.0),
+            child: Text(
+              'Work',
+              style: TextStyle(
+                fontFamily: 'Gilroy ☞',
+                fontSize: 62,
+                color: const Color(0xffffffff),
+                letterSpacing: 6.2,
+                fontWeight: FontWeight.w500,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(-6.0, 2506.0),
+            child: Stack(
+              children: <Widget>[
+                Transform.translate(
+                  offset: Offset(1804.0, 1462.5),
+                  child: SvgPicture.string(
+                    _svg_czxty8,
+                    allowDrawingOutsideViewBox: true,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(453.0, 365.17),
+            child: Text(
+              'hats',
+              style: TextStyle(
+                fontFamily: 'Gilroy ☞',
+                fontSize: 140,
+                color: const Color(0xff000000),
+                letterSpacing: 14,
+                fontWeight: FontWeight.w700,
+                height: 1.5928571428571427,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(1.0, -1460.0),
+            child: Stack(
+              children: <Widget>[
+                Transform.translate(
+                  offset: Offset(140.0, 2192.0),
+                  child: Container(
+                    width: 1885.0,
+                    height: 309.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(27.0),
+                      color: const Color(0xffffffff),
+                      boxShadow: [
+                        BoxShadow(
+                          color: const Color(0xb5d8d8d8),
+                          offset: Offset(0, 0),
+                          blurRadius: 20,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Transform.translate(
+                  offset: Offset(140.0, 2192.0),
+                  child: Container(
+                    width: 58.0,
+                    height: 309.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(29.0),
+                        bottomLeft: Radius.circular(29.0),
+                      ),
+                      color: const Color(0xff000000),
+                      boxShadow: [
+                        BoxShadow(
+                          color: const Color(0xb5d8d8d8),
+                          offset: Offset(0, 10),
+                          blurRadius: 70,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(1.0, -655.0),
+            child: Stack(
+              children: <Widget>[
+                Transform.translate(
+                  offset: Offset(140.0, 2192.0),
+                  child: Container(
+                    width: 1885.0,
+                    height: 309.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(27.0),
+                      color: const Color(0xffffffff),
+                      boxShadow: [
+                        BoxShadow(
+                          color: const Color(0xb5d8d8d8),
+                          offset: Offset(0, 0),
+                          blurRadius: 20,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Transform.translate(
+                  offset: Offset(140.0, 2192.0),
+                  child: Container(
+                    width: 19.0,
+                    height: 309.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(75.0),
+                        bottomLeft: Radius.circular(75.0),
+                      ),
+                      color: const Color(0xff000000),
+                      boxShadow: [
+                        BoxShadow(
+                          color: const Color(0xb5d8d8d8),
+                          offset: Offset(0, 10),
+                          blurRadius: 70,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(-2.0, -1004.0),
+            child: Stack(
+              children: <Widget>[
+                Transform.translate(
+                  offset: Offset(140.0, 2192.0),
+                  child: Container(
+                    width: 1885.0,
+                    height: 309.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(27.0),
+                      color: const Color(0xffffffff),
+                      boxShadow: [
+                        BoxShadow(
+                          color: const Color(0xb5d8d8d8),
+                          offset: Offset(0, 0),
+                          blurRadius: 20,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Transform.translate(
+                  offset: Offset(140.0, 2192.0),
+                  child: Container(
+                    width: 19.0,
+                    height: 309.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(75.0),
+                        bottomLeft: Radius.circular(75.0),
+                      ),
+                      color: const Color(0xff000000),
+                      boxShadow: [
+                        BoxShadow(
+                          color: const Color(0xb5d8d8d8),
+                          offset: Offset(0, 10),
+                          blurRadius: 70,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(-2.0, -306.0),
+            child: Stack(
+              children: <Widget>[
+                Transform.translate(
+                  offset: Offset(140.0, 2192.0),
+                  child: Container(
+                    width: 1885.0,
+                    height: 309.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(27.0),
+                      color: const Color(0xffffffff),
+                      boxShadow: [
+                        BoxShadow(
+                          color: const Color(0xb5d8d8d8),
+                          offset: Offset(0, 0),
+                          blurRadius: 20,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Transform.translate(
+                  offset: Offset(140.0, 2192.0),
+                  child: Container(
+                    width: 19.0,
+                    height: 309.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(75.0),
+                        bottomLeft: Radius.circular(75.0),
+                      ),
+                      color: const Color(0xff000000),
+                      boxShadow: [
+                        BoxShadow(
+                          color: const Color(0xb5d8d8d8),
+                          offset: Offset(0, 10),
+                          blurRadius: 70,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(257.0, 784.67),
+            child: Text(
+              'Chat Bot',
+              style: TextStyle(
+                fontFamily: 'Gilroy ☞',
+                fontSize: 108,
+                color: const Color(0xff000000),
+                letterSpacing: 10.8,
+                fontWeight: FontWeight.w700,
+                height: 1.5925925925925926,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(219.0, 1196.83),
+            child: Text(
+              'Mr. Het',
+              style: TextStyle(
+                fontFamily: 'Gilroy ☞',
+                fontSize: 90,
+                color: const Color(0xff000000),
+                letterSpacing: 9,
+                fontWeight: FontWeight.w600,
+                height: 1.5888888888888888,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(226.0, 1348.83),
+            child: Text(
+              'Hi, i\'ll be there in 5 min.',
+              style: TextStyle(
+                fontFamily: 'Gilroy ☞',
+                fontSize: 64,
+                color: const Color(0xffb9b9b9),
+                letterSpacing: 6.4,
+                fontWeight: FontWeight.w600,
+                height: 1.578125,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(226.0, 1703.83),
+            child: Text(
+              'Here\'s you prescription',
+              style: TextStyle(
+                fontFamily: 'Gilroy ☞',
+                fontSize: 64,
+                color: const Color(0xffb9b9b9),
+                letterSpacing: 6.4,
+                fontWeight: FontWeight.w600,
+                height: 1.578125,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(226.0, 2052.83),
+            child: Text(
+              'i\'ll call you.',
+              style: TextStyle(
+                fontFamily: 'Gilroy ☞',
+                fontSize: 64,
+                color: const Color(0xffb9b9b9),
+                letterSpacing: 6.4,
+                fontWeight: FontWeight.w600,
+                height: 1.578125,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(219.0, 1551.83),
+            child: Text(
+              'Dr. Krish',
+              style: TextStyle(
+                fontFamily: 'Gilroy ☞',
+                fontSize: 90,
+                color: const Color(0xff000000),
+                letterSpacing: 9,
+                fontWeight: FontWeight.w600,
+                height: 1.5888888888888888,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(219.0, 1900.83),
+            child: Text(
+              'Dr. Aryan',
+              style: TextStyle(
+                fontFamily: 'Gilroy ☞',
+                fontSize: 90,
+                color: const Color(0xff000000),
+                letterSpacing: 9,
+                fontWeight: FontWeight.w600,
+                height: 1.5888888888888888,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+const String _svg_94btrn =
+    '<svg viewBox="140.5 104.5 88.5 53.7" ><path transform="translate(140.5, 104.5)" d="M 0 0 L 88.5 0" fill="none" stroke="#000000" stroke-width="10" stroke-miterlimit="4" stroke-linecap="round" /><path transform="translate(140.5, 131.34)" d="M 0 0 L 88.5 0" fill="none" stroke="#000000" stroke-width="10" stroke-miterlimit="4" stroke-linecap="round" /><path transform="translate(140.5, 158.18)" d="M 0 0 L 88.5 0" fill="none" stroke="#000000" stroke-width="10" stroke-miterlimit="4" stroke-linecap="round" /></svg>';
+const String _svg_czxty8 =
+    '<svg viewBox="1804.0 1462.5 92.0 92.0" ><path transform="translate(1850.0, 1462.5)" d="M 0 0 L 0 92" fill="none" stroke="#000000" stroke-width="14" stroke-miterlimit="4" stroke-linecap="round" /><path transform="matrix(0.0, 1.0, -1.0, 0.0, 1896.0, 1508.5)" d="M 0 0 L 0 92" fill="none" stroke="#000000" stroke-width="14" stroke-miterlimit="4" stroke-linecap="round" /></svg>';
